e907df34d61daad9d9a257083e18d3eb10f496d9
Gavin J <rincontek@hotmail.com>
2012-12-29 19:33:52 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmapleApp::Application.config.secret_token = 'c60f2e2099d96e4035ffbfe791e8ec2de8696b5dcfb577b3df74c9cfb930042fba677c07b26188916f390925b4571f4a9a213ba600df9c03d203db8ee0dd684f'
